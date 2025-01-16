85c43699465de63913e9fb16a9379c96cf24c56e
ductaip <dtai858@gmail.com>
2025-01-16 14:40:21 +0700

    style: set up dark mode for pj with shadcn library

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import React from 'react'
 
 export default function Login() {
   return (
     <div>
+      <ModeToggle />
       Login page
     </div>
   )
