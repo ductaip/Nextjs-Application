7119b4395bae56a7c82dd482b7c36d3416d40640
ductaip <dtai858@gmail.com>
2025-01-16 12:58:21 +0700

    style: integrate theme-provider for css variable with shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl roboto-medium text-center">Hi guys how's it going?</h1>
+      <h1 className="text-4xl roboto-medium text-center">Hi guys  it going?</h1>
+      <Link href='/login'>Login</Link>
     </main>
   );
 }
