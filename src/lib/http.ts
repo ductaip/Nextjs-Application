b805dfeadec15f57e3367b9e40e812a19ea6405f
ductaip <dtai858@gmail.com>
2025-01-28 13:04:13 +0700

    fix bug and extend api config

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -14,10 +14,30 @@ class HttpError extends Error {
     }
 }
 
-const request = async (method: 'GET' | 'POST' | 'PUT' | 'DELETE', url: string, options?: CustomOptions | undefined) => {
+const request = async <Response>(method: 'GET' | 'POST' | 'PUT' | 'DELETE', url: string, options?: CustomOptions | undefined) => {
     const body = options?.body ? JSON.stringify(options.body) : undefined
     const baseHeaders = {
         'Content-Type' : 'application/json'
     }
     const baseUrl = options?.baseUrl === undefined ? envConfig.NEXT_PUBLIC_API_ENDPOINT : options.baseUrl
+
+    const fullUrl = url.startsWith('/') ? `${baseUrl}${url}` : `${baseUrl}/${url}`
+
+    const res = await fetch(fullUrl, {
+        ...options,
+        headers: {
+            ...baseHeaders,
+            ...options?.headers
+        },
+        body,
+        method
+    })
+
+    const payload: Response = await res.json()
+    const data = {
+        status: res.status,
+        payload
+    }
+    if(!res.ok) throw new HttpError(data)
+    return data
 }
