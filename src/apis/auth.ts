efba3ff21de5358c172a00c81765ae6bce63036e
ductaip <dtai858@gmail.com>
2025-01-29 18:07:32 +0700

    feat: config auth apis and edit http logic

---
+++ src/apis/auth.ts
@@ -0,0 +1,12 @@
+import http from "@/lib/http";
+import { LoginBodyType, LoginResType, RegisterBodyType, RegisterResType } from "@/schemaValidations/auth.schema";
+
+const authApi = {
+    login: (body: LoginBodyType) => http.post<LoginResType>('/auth/login', body),
+    register: (body: RegisterBodyType) => http.post<RegisterResType>('/auth/register', body),
+    auth: (body: {sessionToken: string}) => http.post('/api/auth', body, {
+        baseUrl: ''
+    }) 
+}
+
+export default authApi
