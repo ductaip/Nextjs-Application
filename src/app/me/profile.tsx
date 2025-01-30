c8d73ad34af89665dd0cadc1e81dfc8ab1472350
ductaip <dtai858@gmail.com>
2025-01-30 21:23:26 +0700

    integrate new profile api

--- src/app/me/profile.tsx
+++ src/app/me/profile.tsx
@@ -1,31 +1,15 @@
 'use client'
 
-import envConfig from "@/config"
 import { useEffect } from "react"
 import { useAppContext } from "../AppProvider"
+import accountApi from "@/apis/account"
 
 export default function Profile() {
     const { sessionToken } = useAppContext()
 
     useEffect(() => {
         const fetchRequest = async () => {
-            const result = await fetch(
-                `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/account/me`,
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization' : `Bearer ${sessionToken}`
-                    }
-                }
-            ).then(async (res) => {
-                const payload = await res.json()
-                const data = {
-                    status: res.status,
-                    payload
-                }
-                if(!res.ok) throw data
-                return data
-            })
+            const result = await accountApi.me(sessionToken)
             console.log('***', result)
         }
         fetchRequest()
